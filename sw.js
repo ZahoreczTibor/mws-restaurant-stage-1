57b1559327463aad7b4e3a22862a2239778cb1a4
ZahoreczTibor <zahorecz.tibor@gmail.com>
2018-05-07 15:08:31 +0200

    update service worker

--- sw.js
+++ sw.js
@@ -58,3 +58,47 @@ self.addEventListener("activate", function(event) {
         })
     );
 });
+
+addEventListener('activate', function (event) {
+    return delete_caches(event);
+});
+
+addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+              .then(function (response) {
+                  // comments from Mozilla MDN
+                  // caches.match() always resolves
+                  // but in case of success response will have value
+                  if (response !== void 0) {
+                      return response;
+                  } else {
+                      return fetch(event.request)
+                          .then((response) => {
+                              let responseClone;
+                              // response may be used only once
+                              // we need to save clone to put one copy in cache
+                              // and serve second one
+                              if (response && response.url) {
+                                  responseClone = response.clone();
+                                  caches
+                                      .open(cache_name)
+                                      .then((cache) => {
+                                                cache.put(event.request, responseClone);
+                                            }
+                                      );
+                              }
+                              return response;
+                          }).catch(function (err) {
+                              console.log(err);
+                              return response
+                          });
+                  }
+              }));
+});
+
+addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+        return this.skipWaiting();
+    }
+});
